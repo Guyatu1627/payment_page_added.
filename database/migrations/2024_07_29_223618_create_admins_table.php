819f5f4eec228f4df84db439df7965ff086ea363
Guyatu1627 <guyatugd.com>
2024-08-26 11:27:49 +0300

    modified

--- database/migrations/2024_07_29_223618_create_admins_table.php
+++ database/migrations/2024_07_29_223618_create_admins_table.php
@@ -4,28 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
+return new class extends Migration {
+    public function up(): void {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email');
+            $table->string('username')->unique();
             $table->string('password');
             $table->integer('is_admin')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('admins');
     }
 };
